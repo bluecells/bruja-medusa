1e20aafe648ae526080ca9cb7050fdf086160209
Blue Cells srl <34393687+bluecells@users.noreply.github.com>
2026-08-25 22:37:26 +0200

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -26,28 +26,25 @@ module.exports = defineConfig({
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
   },
   modules: [
-    { key: "api_key", resolve: "@medusajs/medusa/api-key" },
-    {
-      resolve: "@medusajs/medusa/file",
-      options: {
-        providers: [
-          {
-            resolve: "@medusajs/medusa/file-s3",
-            id: "s3",
-            options: {
-              file_url: process.env.S3_FILE_URL,
-              access_key_id: process.env.S3_ACCESS_KEY_ID,
-              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-              region: process.env.S3_REGION,
-              bucket: process.env.S3_BUCKET,
-              endpoint: process.env.S3_ENDPOINT,
-              additional_client_config: {
-                forcePathStyle: true,
-              },
-            },
+  { key: "api_key", resolve: "@medusajs/medusa/api-key" },
+  {
+    resolve: "@medusajs/medusa/file",
+    options: {
+      providers: [
+        {
+          resolve: "@webbers/cloudflare-r2-medusa/modules/cloudflare-r2",
+          id: "r2",
+          options: {
+            file_url: process.env.S3_FILE_URL,
+            access_key_id: process.env.S3_ACCESS_KEY_ID,
+            secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+            region: "auto",
+            bucket: process.env.S3_BUCKET,
+            endpoint: process.env.S3_ENDPOINT,
           },
-        ],
-      },
+        },
+      ],
     },
-  ],
+  },
+],
 })
