5afbf635846c6640b1a1b7222491f45189a292ea
Blue Cells <bluecells@gmail.com>
2026-09-01 12:46:41 +0200

    R2 storage

--- medusa-config.ts
+++ medusa-config.ts
@@ -27,20 +27,26 @@ module.exports = defineConfig({
   },
   modules: [
   { key: "api_key", resolve: "@medusajs/medusa/api-key" },
-    { resolve: 
-      "@medusajs/medusa/file", 
-         options: { 
-           providers: [ 
-             { resolve: "@medusajs/medusa/file-local", 
-              id: "local", 
-              options: { 
-                upload_dir: "static", 
-                backend_url: `${process.env.BACKEND_URL}/static`, 
-              }, 
-             }, 
-           ], 
-         }, 
+{
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+            },
+          },
+        ],
+      },
     },
+
   ],
 })
 
